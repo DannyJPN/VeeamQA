86be9ef6752e3b5a96e5284b4e908eda1c6ccfb0
DannyJPN <krupadan@email.cz>
2024-05-04 22:53:29 +0200

    Created ProcessMonitor class based on Timer and performing neccessary daptations to the Main method

---
+++ ProcessMonitor/ProcessMonitor/ProcessMonitorTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ProcessMonitor
+{
+    public class ProcessMonitorTimer:Timer
+    {
+        public string ProcessName { get; set; }
+        public int MaximumTTL { get; set; }
+
+        public ProcessMonitorTimer(int secondsInterval) : base(secondsInterval)
+        {
+
+        }
+        public ProcessMonitorTimer(int secondsInterval,string processName,int maximumTTL):this(secondsInterval)
+        {
+            ProcessName = processName;
+            MaximumTTL = maximumTTL;
+        }
+
+    }
+}
--- ProcessMonitor/ProcessMonitor/Program.cs
+++ ProcessMonitor/ProcessMonitor/Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace ProcessMonitor
 {
     internal class Program
     {
+        private static ProcessMonitorTimer processMonitorTimer;
         static void Main(string[] args)
         {
+            string processName = args[1];
+            int maximumTTL = int.Parse(args[2]);
+            int secondsInterval = Convert.ToInt32(args[3]);
+
+            SetProcessMonitorTimer( secondsInterval, processName, maximumTTL);
+
+            Console.WriteLine("\nPress the Enter key to exit the application...\n");
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            Console.ReadLine();
+            processMonitorTimer.Stop();
+            processMonitorTimer.Dispose();
+
+            Console.WriteLine("Terminating the application...");
         }
+
+        private static void SetProcessMonitorTimer(int secondsInterval, string processName, int maximumTTL)
+        {
+            processMonitorTimer = new ProcessMonitorTimer(2000,processName,maximumTTL);
+            processMonitorTimer.Elapsed += OnTimedEvent;
+            processMonitorTimer.AutoReset = true;
+            processMonitorTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+        }
+
     }
 }
